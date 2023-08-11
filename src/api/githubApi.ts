97aea85537941b47af9a58286b501e517924a23f
Daniel Dìaz <danieldiazc413@gmail.com>
2023-08-11 12:47:14 -0500

    Issues y comentarios

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -4,5 +4,6 @@ export const githubApi = axios.create({
     baseURL: "https://api.github.com/repos/facebook/react",
     headers: {
         "Content-Type": "application/json",
+        "Authorization": 'Bearer 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'
     },
 });
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
+import { useIssues } from '../../hooks/useIssues';
+import LoadingIcon from '../../shared/components/LoadingIcon';
 
 
 export const ListView = () => {
 
   const [selectedLabels, setSelectedLabels] = useState<string[]>([])
-  console.log(selectedLabels)
+  const { issuesQuery } = useIssues()
+  
   const onChangeLabel = (labelName: string) => {
     ( selectedLabels.includes(labelName) )
     ? setSelectedLabels( selectedLabels.filter( label => label !== labelName ) )
@@ -17,7 +20,11 @@ export const ListView = () => {
     <div className="row mt-5">
       
       <div className="col-8">
-        <IssueList />
+        {
+          issuesQuery.isLoading 
+          ? (<LoadingIcon />)
+          : (<IssueList issues={ issuesQuery.data || [] }/>)
+        }
       </div>
       
       <div className="col-4">
